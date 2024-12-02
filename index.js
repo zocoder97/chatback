3d67350edf30f5e17805528b08e0dca9b889c0e6
zocoder <andrianambininazocoder@gmail.com>
2024-12-02 14:20:06 +0300

    reglerCores/02

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:4200",
+        origin: "https://monchatapp.netlify.app",
         methods: ["GET", "POST"]
     }
 });
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+    //console.log('User connected:', socket.id);
     // Rejoindre une salle de chat
     socket.on('joinChat', (chatId) => {
         socket.join(chatId);
-        console.log(`${socket.id} joined chat ${chatId}`);
+        //console.log(`${socket.id} joined chat ${chatId}`);
     });
     // Envoyer un message dans une salle spécifique
     socket.on('sendMessage', (data) => {
         io.to(data.chatId).emit('receiveMessage', data);
     });
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+        //console.log('User disconnected:', socket.id);
     });
 });
-server.listen(3055, () => console.log('Server running on http://localhost:3000'));
+server.listen(3055, () => console.log('Server running on http://localhost:3055'));
